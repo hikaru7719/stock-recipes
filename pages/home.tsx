669472808c80e29974fac4bde194a73a07f74879
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2020-12-30 17:29:44 +0900

    カードのレスポンシブ対応

--- pages/home.tsx
+++ pages/home.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
       <div className={"container mx-auto min-h-screen"}>
         <div className={"p-10"}>
           <h1 className="text-3xl font-mono text-gray-600">最近作った料理</h1>
-          <div className="flex flex-col space-y-0 sm:space-y-4 mt-4">
+          <div className="flex flex-col space-y-4 mt-4">
             <div className="flex flex-col sm:flex-row justify-between space-x-0 sm:space-x-4 space-y-2 sm:space-y-0 w-full ">
               <Card title="唐揚げ" />
               <Card title="チキンのトマト煮込み" />
