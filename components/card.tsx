596448391f35704368c950b8d2a6027559de9c7f
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2020-12-31 12:48:25 +0900

    imageのスケルトン生成

--- components/card.tsx
+++ components/card.tsx
@@ -5,6 +5,7 @@ type Props = {
 };
 
 const Card: React.FC<Props> = (props) => {
+  const [isLoading, setIsLoading] = React.useState(true);
   return (
     <div className={"w-full sm:w-1/3 mb-4 px-2"}>
       <div className={"bg-white shadow-md rounded-md h-72 p-6"}>
@@ -12,8 +13,17 @@ const Card: React.FC<Props> = (props) => {
           <p className="text-mono text-xl text-gray-600">{props.title}</p>
           <p className="text-mono text-gray-600">2020/12/10</p>
         </div>
-        <div className="relative w-full h-5/6">
-          <Image src="/tamago.jpg" layout="fill" />
+        <div
+          className={`relative w-full h-5/6 ${
+            isLoading ? "bg-gray-300 animate-pulse" : ""
+          }`}
+        >
+          <Image
+            src="/tamago.jpg"
+            layout="fill"
+            className={isLoading ? "hidden" : ""}
+            onLoad={() => setIsLoading(false)}
+          />
         </div>
       </div>
     </div>
