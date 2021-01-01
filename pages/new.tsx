efc5ee587f2c2e7a3a6fedc6869fd348cfcd1008
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2021-01-01 23:17:28 +0900

    作成画面の雛形完成

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
+import { useForm } from "react-hook-form";
 import Header from "../components/header";
 
 const New: React.FC = () => {
+  const { register, handleSubmit } = useForm();
+  const [fileName, setFileName] = React.useState("");
+
   return (
     <div className="bg-gray-50 min-h-screen ">
       <Header />
@@ -9,14 +13,22 @@ const New: React.FC = () => {
         <div className={"p-10"}>
           <h1 className="text-3xl font-mono text-gray-600">料理の登録</h1>
           <div className={"mt-6"}>
-            <form className="flex flex-col">
+            <form
+              className="flex flex-col"
+              onSubmit={handleSubmit((data) => {
+                console.log(data.image);
+                console.log(JSON.stringify(data));
+              })}
+            >
               <div className="flex flex-col my-3">
                 <label className="mb-1 text-mono text-gray-600 text-lg">
                   料理名
                 </label>
                 <input
+                  name="recipeName"
                   type="text"
                   className="w-full sm:w-2/3 h-10 ring-1 ring-gray-200 rounded-md px-2 text-mono text-gray-700"
+                  ref={register}
                 />
               </div>
               <div className="flex flex-col my-3">
@@ -24,8 +36,10 @@ const New: React.FC = () => {
                   レシピURL
                 </label>
                 <input
+                  name="url"
                   type="text"
                   className="w-full sm:w-2/3 h-10 ring-1 ring-gray-200 rounded-md px-2 text-mono text-gray-700"
+                  ref={register}
                 />
               </div>
               <div className="flex flex-col my-3">
@@ -33,20 +47,48 @@ const New: React.FC = () => {
                   日付
                 </label>
                 <input
+                  name="date"
                   type="date"
                   className="w-full sm:w-1/6 h-10 ring-1 ring-gray-200 rounded-md px-2 text-mono text-gray-700"
+                  ref={register}
                 />
               </div>
               <div className="flex flex-col my-3">
                 <label className="mb-1 text-mono text-gray-600 text-lg">
                   時間帯
                 </label>
-                <select className="w-full sm:w-1/6 h-10 ring-1 ring-gray-200 rounded-md px-2 text-mono text-gray-700">
+                <select
+                  name="spanOfTime"
+                  className="w-full sm:w-1/6 h-10 ring-1 ring-gray-200 rounded-md px-2 text-mono text-gray-700"
+                  ref={register}
+                >
                   <option>朝</option>
                   <option>昼</option>
                   <option>夜</option>
                   <option>それ以外</option>
                 </select>
+              </div>
+              <div className="flex flex-col my-3">
+                <label className="mb-1 text-mono text-gray-600 text-lg">
+                  写真
+                </label>
+                <input
+                  type="text"
+                  disabled
+                  className="w-full sm:w-1/3 h-10 ring-1 ring-gray-200 rounded-md px-2 text-mono text-gray-700 mb-3 bg-white"
+                  placeholder="選択したファイル名が表示されます。"
+                  value={fileName}
+                />
+                <label className="w-full sm:w-2/12 h-10 ring-1 ring-gray-200 rounded-md bg-gray-400 text-white text-center pt-2">
+                  ファイルを選択
+                  <input
+                    name="image"
+                    type="file"
+                    className="hidden"
+                    ref={register}
+                    onChange={(e) => setFileName(e.target?.files[0]?.name)}
+                  />
+                </label>
               </div>
               <button
                 className="text-lg my-7 w-full sm:w-1/12 h-10 ring-1 ring-gray-200 rounded-md bg-red-300 text-white"
