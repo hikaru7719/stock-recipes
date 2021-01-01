d6f75cda0b79b67c84a5bdbd405e274d2be19fcf
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2021-01-01 14:04:23 +0900

    カードのレイアウトを変更

--- components/card.tsx
+++ components/card.tsx
@@ -7,8 +7,8 @@ type Props = {
 const Card: React.FC<Props> = (props) => {
   const [isLoading, setIsLoading] = React.useState(true);
   return (
-    <div className={"w-full sm:w-1/3 mb-4 px-2"}>
-      <div className={"bg-white shadow-md rounded-md h-96 p-6"}>
+    <div className={"w-full sm:w-1/4 mb-4 px-2"}>
+      <div className={"bg-white shadow-md rounded-md h-72 p-6"}>
         <div className="flex justify-between mb-3">
           <p className="text-mono text-xl text-gray-600">{props.title}</p>
           <p className="text-mono text-gray-600">2020/12/10</p>
