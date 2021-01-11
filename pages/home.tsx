9857e17afa303fd7b1c9b639dbb666b102b1792c
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2021-01-11 17:53:33 +0900

    APIコールの間はFakeCardを表示するように修正

--- pages/home.tsx
+++ pages/home.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import useSWR from "swr";
 import CardList from "../components/cardList";
 import Header from "../components/header";
+import LoadingCardList from "../components/loadingCardList";
 import { download, findRecipes, getCurrentUserUid } from "../firebase";
 
 const Home: React.FC = () => {
@@ -31,7 +32,17 @@ const Home: React.FC = () => {
       </div>
     </div>
   ) : (
-    <div>Loading ...</div>
+    <div className="bg-gray-50 min-h-screen ">
+      <Header />
+      <div className={"container mx-auto min-h-screen"}>
+        <div className={"p-10"}>
+          <h1 className="text-3xl font-mono text-gray-600">最近作った料理</h1>
+          <div className={"mt-6 -mx-2"}>
+            <LoadingCardList />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
