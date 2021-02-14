0316e991f811ca2f8b581eb779b237d76dd15f3b
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2021-02-14 18:22:39 +0900

    不要なimportの削除

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,8 +1,5 @@
-import { useRouter } from "next/router";
 import * as React from "react";
-import { useDispatch } from "react-redux";
 import { getCurrentUserUid } from "./firebase";
-import { reset } from "./store/messageSlice";
 
 export function useUser(func: () => void): string {
   const uid = getCurrentUserUid();
